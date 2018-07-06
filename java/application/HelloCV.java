b90f034c1d3768d402a33a3a51ef5917290cfc07
Arina Umushkina <Arina_Umushkina@epam.com>
2018-07-06 11:00:52 +0300

    Add first commit with lib opencv maven

---
+++ java/application/HelloCV.java
@@ -0,0 +1,20 @@
+package application;
+
+import org.opencv.core.Core;
+
+public class HelloCV {
+
+    static {
+        nu.pattern.OpenCV.loadShared();
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Core.VERSION);
+        System.out.println(Core.VERSION_MAJOR);
+        System.out.println(Core.VERSION_MINOR);
+        System.out.println(Core.VERSION_REVISION);
+        System.out.println(Core.NATIVE_LIBRARY_NAME);
+        System.out.println(Core.getBuildInformation());
+    }
+}
